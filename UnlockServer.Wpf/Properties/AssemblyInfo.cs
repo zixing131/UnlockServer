67a911e899d0f9fd099cdcc3a54d9ab41b6bd6cc
zixing <1311817771@qq.com>
2026-09-15 19:03:24 +0800

    Update AssemblyInfo.cs

--- UnlockServer.Wpf/Properties/AssemblyInfo.cs
+++ UnlockServer.Wpf/Properties/AssemblyInfo.cs
@@ -20,6 +20,6 @@ using System.Windows;
     ResourceDictionaryLocation.SourceAssembly
 )]
 
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
 
